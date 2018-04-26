25f92191cff9b4a05962757bbd0eaa67672380c8
mafanghua <980992720@qq.com>
2018-04-26 19:19:04 +0800

    no message

---
+++ BloomBeauty/BloomBeauty/UIAlertController+autorotate.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertController+autorotate.h
+//  装扮灵
+//
+//  Created by EEKA on 2018/4/26.
+//  Copyright © 2018年 EEKA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertController (autorotate)
+
+@end
---
+++ BloomBeauty/BloomBeauty/UIAlertController+autorotate.m
@@ -0,0 +1,18 @@
+//
+//  UIAlertController+autorotate.m
+//  装扮灵
+//
+//  Created by EEKA on 2018/4/26.
+//  Copyright © 2018年 EEKA. All rights reserved.
+//
+
+#import "UIAlertController+autorotate.h"
+
+@implementation UIAlertController (autorotate)
+
+-(BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+@end
